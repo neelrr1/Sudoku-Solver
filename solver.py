94dd2e3645b509ef18431ba2bb2a039150beec05
Neel Raja <neelrr1@gmail.com>
2020-03-22 17:11:36 -0700

    Add solve button and backtracking visualization

--- solver.py
+++ solver.py
@@ -16,31 +16,32 @@ from pygame import *
 # ]
 
 # Easy Example
-board = [
-    [6, 0, 0, 0, 5, 0, 0, 4, 1],
-    [0, 5, 4, 0, 0, 8, 0, 3, 0],
-    [7, 0, 3, 0, 9, 0, 0, 0, 8],
-    [2, 4, 0, 0, 0, 1, 0, 6, 5],
-    [0, 0, 7, 0, 0, 0, 2, 0, 0],
-    [5, 6, 0, 9, 0, 0, 0, 7, 3],
-    [1, 0, 0, 0, 7, 0, 3, 0, 4],
-    [0, 8, 0, 5, 0, 0, 7, 9, 0],
-    [3, 7, 0, 0, 4, 0, 0, 0, 2]
-]
-
-# Harder Example
 # board = [
-#     [9, 0, 3, 8, 0, 0, 0, 0, 0],
-#     [1, 0, 0, 0, 4, 0, 5, 0, 0],
-#     [0, 0, 2, 0, 0, 0, 0, 0, 0],
-#     [3, 0, 0, 0, 0, 7, 4, 6, 8],
-#     [0, 0, 0, 6, 0, 9, 0, 0, 0],
-#     [2, 7, 6, 4, 0, 0, 0, 0, 9],
-#     [0, 0, 0, 0, 0, 0, 8, 0, 0],
-#     [0, 0, 1, 0, 2, 0, 0, 0, 3],
-#     [0, 0, 0, 0, 0, 5, 7, 0, 6]
+#     [6, 0, 0, 0, 5, 0, 0, 4, 1],
+#     [0, 5, 4, 0, 0, 8, 0, 3, 0],
+#     [7, 0, 3, 0, 9, 0, 0, 0, 8],
+#     [2, 4, 0, 0, 0, 1, 0, 6, 5],
+#     [0, 0, 7, 0, 0, 0, 2, 0, 0],
+#     [5, 6, 0, 9, 0, 0, 0, 7, 3],
+#     [1, 0, 0, 0, 7, 0, 3, 0, 4],
+#     [0, 8, 0, 5, 0, 0, 7, 9, 0],
+#     [3, 7, 0, 0, 4, 0, 0, 0, 2]
 # ]
 
+# Harder Example
+board = [
+    [9, 0, 3, 8, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 4, 0, 5, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0, 0, 0],
+    [3, 0, 0, 0, 0, 7, 4, 6, 8],
+    [0, 0, 0, 6, 0, 9, 0, 0, 0],
+    [2, 7, 6, 4, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 0, 0, 8, 0, 0],
+    [0, 0, 1, 0, 2, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 5, 7, 0, 6]
+]
+
+# Possible numbers on the game board
 nums = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 # Game logic
@@ -131,10 +132,10 @@ def nextSquare(i, j):
 def hasNextSquare(i, j):
     return not (i == 8 and j == 8)
 
-def solve():
-    solveHelper(0, 0, 0)
+def solve(ui=False):
+    solveHelper(0, 0, 0, ui=ui)
 
-def solveHelper(i, j, numsIndex):
+def solveHelper(i, j, numsIndex, ui=False):
     nums = list(getValidNums(i, j))
     # print(i, j, nums)
 
@@ -142,21 +143,26 @@ def solveHelper(i, j, numsIndex):
         if (board[i][j] == 0):
             if (numsIndex >= len(nums)): return False # Cannot proceed, must backtrack
             board[i][j] = nums[numsIndex]
+
+            if ui:
+                drawCell(i, j)
+
             (nextI, nextJ) = nextSquare(i, j)
-            if (not solveHelper(nextI, nextJ, 0)):
+            if (not solveHelper(nextI, nextJ, 0, ui)):
                 board[i][j] = 0
-                return solveHelper(i, j, numsIndex + 1)
+                return solveHelper(i, j, numsIndex + 1, ui)
             else:
                 return True # Solution was found?
         else:
             (nextI, nextJ) = nextSquare(i, j)
-            return solveHelper(nextI, nextJ, 0)
+            return solveHelper(nextI, nextJ, 0, ui)
     else:
         if (board[i][j] == 0): board[i][j] = nums[numsIndex]
         return True # Solved sudoku board
 
 # UI
-def drawBoxes(screen, size, color):
+def drawBoxes(color):
+    size = boxSize
     box = Rect(0, 0, size, size)
 
     for i in range(3):
@@ -165,7 +171,8 @@ def drawBoxes(screen, size, color):
             box.top = i * box.height
             pg.draw.rect(screen, color, box, 2)
 
-def drawCells(screen, size, color):
+def drawCells(color):
+    size = cellSize
     cell = Rect(0, 0, size, size)
 
     for i in range(9):
@@ -178,25 +185,83 @@ def drawCells(screen, size, color):
             font = freetype.SysFont('arial', size)
             value = ""
             if board[i][j] != 0: value = str(board[i][j]) 
-            font.render_to(screen, (j * cell.width + (size / 4), i * cell.height + (size / 8)), value, color)
-            
+            font.render_to(screen, (j * size + (size // 4), i * size + (size // 8)), value, color)
 
+def drawCell(i, j):
+    size = cellSize
+    color = cellColor
+    fontColor = solvedColor
+
+    cell = Rect(0, 0, size, size)
+    cell.left = j * cell.width
+    cell.top = i * cell.height
+    pg.draw.rect(screen, white, cell, 0)
+    pg.draw.rect(screen, color, cell, 2)
+    drawBoxes(red)
+
+    # Draw values
+    font = freetype.SysFont('arial', size)
+    value = ""
+    if board[i][j] != 0: value = str(board[i][j]) 
+    font.render_to(screen, (j * size + (size // 4), i * size + (size // 8)), value, fontColor)
+
+    pg.display.update()
+    # pg.time.Clock().tick(60)
+
+def clearCell(i, j):
+    size = cellSize
+    color = cellColor
+
+    cell = Rect(0, 0, size, size)
+    cell.left = j * cell.width
+    cell.top = i * cell.height
+    pg.draw.rect(screen, white, cell, 0)
+    pg.draw.rect(screen, color, cell, 2)
+    drawBoxes(red)
+
+    pg.display.update()
+    # pg.time.Clock().tick(60)
+            
 if __name__ == "__main__":
     # Initialization
     pg.init()
-    gameWidth = 900
-    gameHeight = 900
-    screen = pg.display.set_mode((gameWidth,gameHeight))
+    gameWidth = 450 # Make sure divisible by 9
+    gameHeight = 450
+    screenWidth = gameWidth + gameWidth // 5 # 120% of gameWidth for input, buttons, etc.
+    pg.display.set_caption('Sudoku Solver')
+    # global screen
+    screen = pg.display.set_mode((screenWidth,gameHeight))
     # TODO: Set title of window
     screen.fill(Color('white'))
 
+    # Colors
+    black = Color('black')
+    red = Color('red')
+    blue = Color('blue')
+    white = Color('white')
+
     # Constants
-    line_width = 5
-    cell = Rect(0, 0, 100, 100)
+    cellSize = gameWidth // 9
+    boxSize = gameWidth // 3
+    cellColor = black
+    boxColor = red
+    givenColor = black
+    solvedColor = blue
+
+    # Objects
+    solveText = "Solve!"
+    solveFont = freetype.SysFont('arial', gameHeight // 20)
+    solvePos = (gameWidth + 10, gameHeight // 2)
+    solveRect = solveFont.get_rect(solveText)
+    solveRect = solveRect.inflate(10, 10)
+    solveRect.centery = solvePos[1] + 5
+    solveRect.left = solvePos[0] - 5
 
     # Draw elements
-    drawCells(screen, gameWidth // 9, Color('black'))
-    drawBoxes(screen, gameWidth // 3, Color('red'))
+    drawCells(black)
+    drawBoxes(red)
+    pg.draw.rect(screen, red, solveRect)
+    solveFont.render_to(screen, solvePos, solveText, black)
 
     pg.display.update()
 
@@ -206,6 +271,16 @@ if __name__ == "__main__":
             if i.type == pg.QUIT:
                 running = False
                 pg.quit()
+            if i.type == pg.MOUSEBUTTONDOWN:
+                mouse_pos = i.pos  # gets mouse position
+
+                # Clicked on Solve button
+                if solveRect.collidepoint(mouse_pos):
+                    # prints current location of mouse
+                    # UI Info: (cell size, cell color, font color)
+                    solve(ui=True) # Blocks other UI updates
+                    # drawCells(black)
+                    # drawBoxes(red)
 
         if not running: break
         pg.display.update()
